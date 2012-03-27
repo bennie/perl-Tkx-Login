f4af5f8d422d5ef1e70d9cc8997b9b79b4f993d9
bennie@cpan.org <bennie@cpan.org>
2012-03-27 23:26:28 +0000

    initial revision.

--- lib/Tkx/Login.pm
+++ lib/Tkx/Login.pm
@@ -1,10 +1,48 @@
+$Tkx::Login::VERSION='VERSIONTAG';
 
-use Tkx::Login;
+use Tkx;
 use strict;
 
-sub new {
-  my $class = shift @_;
-  my $self = {};
-  bless $self, $class;
-  return $self;
+package Tkx::Login;
+
+sub askpass {
+  my $gotpass = 0;
+
+  my $mw = shift @_;
+  my $user = shift @_;
+  my $pass = shift @_;
+
+  my $win = $mw->new_toplevel();
+  $win->g_wm_title("Login");
+
+  my $label = $win->new_ttk__label(-text => "Please enter Username and Password for ASUP." );
+  $label->g_grid;
+
+  $win->new_ttk__label(-text => "Username:" )->g_grid;
+
+  my $name_entry = $win->new_ttk__entry(-textvariable => \$user);
+  $name_entry->g_grid;
+
+  $win->new_ttk__label(-text => "Password:" )->g_grid;
+
+  my $pass_entry = $win->new_ttk__entry(-textvariable => \$pass, -show => '*');
+  $pass_entry->g_grid;
+
+  my $ok = $win->new_button(
+    -text => 'Ok',
+    -command => sub {
+       #message("Got: ".$name_entry->get );
+       $gotpass++;
+       $win->g_destroy;
+    },
+  );
+  $ok->g_grid;
+
+  while ( $gotpass < 1 ) {
+    Tkx::update();
+  }
+
+  return ( $user, $pass );
 }
+
+1;
